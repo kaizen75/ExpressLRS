62598a327e3c07fa7c02318a705b13145e9a996d
Cube Sine <cubesine@hotmail.com>
2024-04-21 19:07:20 +0200

    added SX1262 related code

--- src/include/common.h
+++ src/include/common.h
@@ -9,6 +9,8 @@
 #include "LR1121Driver.h"
 #elif defined(RADIO_SX128X)
 #include "SX1280Driver.h"
+#elif defined(RADIO_SX126X)
+#include "SX126xDriver.h"
 #else
 #error "Radio configuration is not valid!"
 #endif
@@ -251,6 +253,12 @@ extern SX127xDriver Radio;
 #define RATE_DUALBAND_BINDING 9 // 2.4GHz 50Hz
 
 extern LR1121Driver Radio;
+
+#elif defined(RADIO_SX126X)
+#define RATE_MAX 5
+#define RATE_BINDING RATE_LORA_50HZ
+
+extern SX126xDriver Radio;
 
 #elif defined(RADIO_SX128X)
 #define RATE_MAX 10     // 2xFLRC + 2xDVDA + 4xLoRa + 2xFullRes
